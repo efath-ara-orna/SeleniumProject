0a80c318c7d7c0c8e4441e03d86cd7e0a906eafd
efath <efath.orna172@gmail.com>
2022-04-19 14:19:30 +0600

    Web Automation project On my store

---
+++ src/main/java/it/com/webAutomationSeleniumProject/App.java
@@ -0,0 +1,13 @@
+package it.com.webAutomationSeleniumProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
